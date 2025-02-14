60d0b921956543302277f18e05a0073afcef8bdd
hombiochen <hombiochen@163.com>
2025-02-14 17:49:21 +0800

    add:requirements.txt

--- tool_call_test.py
+++ tool_call_test.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
 from langchain_deepseek import ChatDeepSeek
 
 load_dotenv()
-
 
 model = ChatDeepSeek(
     model="deepseek-chat",
@@ -13,17 +14,29 @@ model = ChatDeepSeek(
     # other params...
 )
 
+@tool
 def multiply(a: int, b: int) -> int:
-    """Multiply a and b.
-
-    Args:
-        a: first int
-        b: second int
-    """
+    """Multiply two numbers."""
     return a * b
 
-llm_with_tools = model.bind_tools([multiply])
-result = llm_with_tools.invoke("Hello world!")
-print(result)
-result = llm_with_tools.invoke("What is 2 multiplied by 3?")
-print(result)
+tools = [multiply]
+
+llm_with_tools = model.bind_tools(tools)
+
+query = "What is 2 multiplied by 3?"
+messages = [HumanMessage(query)]
+
+ai_msg = llm_with_tools.invoke(messages)
+messages.append(ai_msg)
+
+# pass tool outputs to chat models
+for tool_call in ai_msg.tool_calls:
+    selected_tool = {"multiply": multiply}[tool_call["name"].lower()]
+    tool_msg = selected_tool.invoke(tool_call)
+    messages.append(tool_msg)
+
+ai_msg = model.invoke(messages)
+messages.append(ai_msg)
+
+for message in messages:
+    print(message)
